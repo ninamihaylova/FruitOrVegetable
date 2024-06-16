d53177c4603a9286e6497d44cc9f75c65e62733f
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 16:36:41 +0300

    Initial commit

---
+++ FruitOrVegetable/Program.cs
@@ -0,0 +1,18 @@
+﻿class FruitOrVegetable {
+    public static void Main (string[]args) {
+    
+        string product=Console.ReadLine();
+        if (product=="banana" || 
+            product=="apple"|| product=="kiwi" || product=="cherry" || product=="lemon") {
+            Console.WriteLine("fruit");
+        }
+        else if (product==
+                 "cucumber" || product=="pepper" || product=="carrot") {
+            Console.WriteLine("vegetable") ;
+        }
+        else {
+            Console.WriteLine("unknown");
+  
+        }
+    }
+}
